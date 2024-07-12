11351de4c94b419f01e030ad92e3ea9897641b3c
John Doe <johndoe@example.com>
2024-07-12 11:55:00 -0300

    demostracao

---
+++ Myoperadores.py
@@ -0,0 +1,9 @@
+# Demostraçao de expressões completos
+
+#Declaração de variaveis que iremos usar...
+x= 10; y= 20; z=30
+
+valor= x*(y/10+z/10)-25
+print("O resultado da expressão #1 é",valor)
+
+valor=
---
+++ myCalcs.py
@@ -0,0 +1,15 @@
+#demostração do uso de operadores
+X = int(input("digite um numero"))
+Y = int(input("digite um numero"))
+
+soma= X + Y
+print("O resultado da subtração é:",soma)
+
+subtraçao= X-Y
+print("O resultado da subtração é:",subtraçao)
+
+multiplicacao = X * Y
+print("O resultado da multiplicação é",multiplicacao)
+
+divisao = X / Y
+print ("O resultado da divisão é",divisao)
