26c1940ba1c88334dfc271ce7374b38458564dec
John Doe <johndoe@example.com>
2024-07-15 09:27:07 -0300

    123

---
+++ MyCalcs2.py
@@ -0,0 +1,19 @@
+#Demostraçao do uso de operadores...
+
+x= int(input("digite um numero"))
+y=int(input("digite um numero"))
+
+soma= x+y
+print("O resultado da soma é :",soma)
+
+subtração =x-y
+print("O resultado da subtraçao é:", subtração)
+
+multiplicação= x*y
+print("O resultado da multiplicaçao é:",multiplicação)
+
+divisão= x/y
+print("O resultado da divisão é:",divisão)
+
+
+
---
+++ MyMath.py
@@ -0,0 +1,14 @@
+# Demostração do uso de operadores...
+#Criar um programa que faça o somatório de valores e tirea sua média 
+
+x=int(input("Digite o valor 1:"))
+y= int (input("DIGITE O VALOR 2:"))
+z= int(input("dIGITE O VALOR 3:)"))
+soma= x +y +z
+media= soma /3
+
+print("A soma é:",soma)
+print(f"A media é {media}")
+
+
+
